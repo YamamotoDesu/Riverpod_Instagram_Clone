5e3b8660af92f00502ff49525b178ab48494cdd8
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-04-17 10:13:48 +0900

    authentication setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:riverpod_instagram_clone/state/auth/backend/authenticator.dart';
 import 'firebase_options.dart';
+
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,10 +19,9 @@ void main() async {
       title: 'Flutter Demo',
       themeMode: ThemeMode.dark,
       darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.blueGrey,
-        indicatorColor: Colors.blueGrey
-      ),
+          brightness: Brightness.dark,
+          primarySwatch: Colors.blueGrey,
+          indicatorColor: Colors.blueGrey),
       theme: ThemeData(
         brightness: Brightness.dark,
         primarySwatch: Colors.blue,
@@ -33,7 +39,29 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: const Text('Home Page'),
-    ));
+          title: const Text('Home Page'),
+        ),
+        body: Column(
+          children: [
+            TextButton(
+              onPressed: () async {
+                final result = await Authenticator().loginWithGoogle();
+                result.log();
+              },
+              child: const Text(
+                'Sign In with Google',
+              ),
+            ),
+            TextButton(
+              onPressed: () async {
+                final result = await Authenticator().loginWithFacebook();
+                result.log();
+              },
+              child: const Text(
+                'Sign In with Facebook',
+              ),
+            ),
+          ],
+        ));
   }
 }
